5cf96f64952dc5d834bc9e21abfa084f7c5ebde8
Soliderik <soliderik@gmail.com>
2021-01-25 02:26:22 -0600

    nuevas paginas
    se agregaron html.

--- js/js.js
+++ js/js.js
@@ -1,31 +1,31 @@
-let saludos = "hola Saidy :)"
-let mensaje1 = "¿Cómo estás?"
-let mensaje2 = "¿que estás haciendo?"
-let mensaje3 = "que tengas una bonita noche :)"
-let rsthtml = document.getElementById("rst")
-let continthtml = document.querySelector(".contint")
-let btnsaludo = document.getElementById("saludo")
-let btnmensaje1 = document.getElementById("msg1")
-let btnmensaje2 = document.getElementById("msg2")
-let btnmensaje3 = document.getElementById("msg3")
-let msgoriginal = continthtml.textContent
+// let saludos = "hola Annie :)"
+// let mensaje1 = "¿Cómo estás?"
+// let mensaje2 = "¿que estás haciendo?"
+// let mensaje3 = "que tengas una bonita noche :)"
+// let rsthtml = document.getElementById("rst")
+// let continthtml = document.querySelector(".contint")
+// let btnsaludo = document.getElementById("saludo")
+// let btnmensaje1 = document.getElementById("msg1")
+// let btnmensaje2 = document.getElementById("msg2")
+// let btnmensaje3 = document.getElementById("msg3")
+// let msgoriginal = continthtml.textContent
 
-function actualizarContenido (textoFinal) {
-    continthtml.textContent = textoFinal
-}
+// function actualizarContenido (textoFinal) {
+//     continthtml.textContent = textoFinal
+// }
 
-btnmensaje1.addEventListener("click", function(){
-    actualizarContenido(mensaje1)
-})
-btnmensaje2.addEventListener("click", function(){
-    actualizarContenido(mensaje2)
-})
-btnmensaje3.addEventListener("click", function(){
-    actualizarContenido(mensaje3)
-})
-btnsaludo.addEventListener("click", function(){
-    actualizarContenido(saludos)
-} )
-rsthtml.addEventListener("click", function(){
-    actualizarContenido(msgoriginal)
-} )
+// btnmensaje1.addEventListener("click", function(){
+//     actualizarContenido(mensaje1)
+// })
+// btnmensaje2.addEventListener("click", function(){
+//     actualizarContenido(mensaje2)
+// })
+// btnmensaje3.addEventListener("click", function(){
+//     actualizarContenido(mensaje3)
+// })
+// btnsaludo.addEventListener("click", function(){
+//     actualizarContenido(saludos)
+// } )
+// rsthtml.addEventListener("click", function(){
+//     actualizarContenido(msgoriginal)
+// } )
